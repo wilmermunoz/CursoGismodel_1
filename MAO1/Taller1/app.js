f10a2ae6298923556a46c4c9a84f9253ecdb0b76
maurorin@gmail.com <maurorin@gmail.com>
2017-09-25 18:08:19 -0500

    Versión del lunes 6pm

---
+++ MAO1/Taller1/app.js
@@ -0,0 +1,58 @@
+
+$("#id_1").text('   ');
+$(".Clase1").text(' mi nueva clase  ');
+
+$(".Clase2").css("color", "red");
+$(".Clase2").css("font-size", "50px");
+
+$("#boton1").on('click', function(){
+    var valor2 =$(this).attr("midato")
+    var valor1 = $("#boton1").attr('data');
+    alert(valor1);
+    alert(valor2);
+    alert('HHOOLLAAA');
+
+});
+
+function boton2(variable1, variable2){
+    alert('BOTON 2');
+
+}
+
+$(".boton3").on('click', function(){
+    alert('HHOOLLAAAAAAAAAAAA');});
+
+
+$(".boton4").on('click', function(){
+    alert('HHOOLLAAAAAAAAA BOTON4');});
+
+
+
+for(var i=1; i<50; i++){
+
+//    var mi_html ="<input type='button' value='boton_"+i+"'>";
+ //   console.log(html);
+       var mi_html ="<input onclick='function_botones("+i+")' type='button' value='boton_"+i+"'>";
+
+    $(".misbotones").append(mi_html);
+
+
+    function function_botones(valor){
+        console.log(valor)
+    }
+}
+
+$(".misbotones").append(mi_html);
+
+
+for(var i=1; i<50; i++){
+
+      var mi_html ="<input class='botonciclo'  midato= '"+i+"' type='button' value='boton_"+i+"'>";
+
+    $(".misbotones").append(mi_html);
+
+}
+$(".botonciclo").on('click', function(){
+    console.log(midato);
+
+})
